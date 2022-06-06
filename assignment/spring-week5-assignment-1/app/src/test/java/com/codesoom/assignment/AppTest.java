e39ed2e90cb220ef3e9fdfc0da4de7f1192480d7
yongjunleeme <chesidyong@gmail.com>
2022-06-06 15:54:30 +0900

    5주차 과제 추가

---
+++ assignment/spring-week5-assignment-1/app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class AppTest {
+    @Test
+    void appHasAGreeting() {
+    }
+}
